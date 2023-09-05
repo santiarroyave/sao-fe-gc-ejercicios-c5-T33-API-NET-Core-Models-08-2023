31bc13a8a64e4a42ffa1b7f782762225b5de2efe
santiarroyave <santi98cv@hotmail.com>
2023-09-05 13:12:39 +0200

    ex02

---
+++ ex02/Controllers/EmpleadosController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ex02.Data;
+using ex02.Models;
+
+namespace ex02.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmpleadosController : ControllerBase
+    {
+        private readonly MyDbContext _context;
+
+        public EmpleadosController(MyDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Empleados
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Empleado>>> GetEmpleados()
+        {
+          if (_context.Empleados == null)
+          {
+              return NotFound();
+          }
+            return await _context.Empleados.ToListAsync();
+        }
+
+        // GET: api/Empleados/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Empleado>> GetEmpleado(string id)
+        {
+          if (_context.Empleados == null)
+          {
+              return NotFound();
+          }
+            var empleado = await _context.Empleados.FindAsync(id);
+
+            if (empleado == null)
+            {
+                return NotFound();
+            }
+
+            return empleado;
+        }
+
+        // PUT: api/Empleados/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutEmpleado(string id, Empleado empleado)
+        {
+            if (id != empleado.dni)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(empleado).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EmpleadoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Empleados
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Empleado>> PostEmpleado(Empleado empleado)
+        {
+          if (_context.Empleados == null)
+          {
+              return Problem("Entity set 'MyDbContext.Empleados'  is null.");
+          }
+            _context.Empleados.Add(empleado);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (EmpleadoExists(empleado.dni))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetEmpleado", new { id = empleado.dni }, empleado);
+        }
+
+        // DELETE: api/Empleados/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEmpleado(string id)
+        {
+            if (_context.Empleados == null)
+            {
+                return NotFound();
+            }
+            var empleado = await _context.Empleados.FindAsync(id);
+            if (empleado == null)
+            {
+                return NotFound();
+            }
+
+            _context.Empleados.Remove(empleado);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool EmpleadoExists(string id)
+        {
+            return (_context.Empleados?.Any(e => e.dni == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ ex02/Data/MyDbContext.cs
@@ -0,0 +1,37 @@
+﻿using ex02.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ex02.Data
+{
+    public class MyDbContext :DbContext
+    {
+        public DbSet<Departamento> Departamentos { get; set;}
+        public DbSet<Empleado> Empleados { get; set;}
+
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Departamento>(departamento =>
+            {
+                departamento.ToTable("Departamentos");
+                departamento.HasKey(p => p.codigo);
+
+                departamento.Property(p => p.nombre).HasMaxLength(100);
+            });
+
+            modelBuilder.Entity<Empleado>(empleado =>
+            {
+                empleado.ToTable("Empleados");
+                empleado.HasKey(p => p.dni);
+
+                empleado.Property(p => p.dni).HasMaxLength(8);
+                empleado.Property(p => p.nombre).HasMaxLength(100);
+                empleado.Property(p => p.apellidos).HasMaxLength(255);
+                empleado.Property(p => p.fk_departamento).HasColumnName("Departamento");
+
+                empleado.HasOne(p => p.v_departamento).WithMany(p => p.v_empleados).HasForeignKey(p => p.fk_departamento);
+            });
+        }
+    }
+}
